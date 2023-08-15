705d6f9256be25595e023114880e0fa891737a95
gulyaeve <baddman@list.ru>
2023-08-15 11:24:59 +0300

    Added thumbnail to png

--- main.py
+++ main.py
@@ -3,7 +3,16 @@ from PIL import Image
 
 def main():
     im = Image.open("screenshot.jpg")
-    im.show()
+    print(im.format, im.size, im.mode)
+
+    size = (128, 128)
+    outfile = "screenshot_thumbnail.png"
+
+    im.thumbnail(size)
+    im.save(outfile, "PNG")
+
+    newfile = Image.open(outfile)
+    print(newfile.format, newfile.size, newfile.mode)
 
 
 if __name__ == '__main__':
